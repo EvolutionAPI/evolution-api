bbc2b8a3960e89e65d506f2f59429481779b9dfc
AdsonCicilioti <eu@adsonagencia.com>
2023-08-26 00:29:58 -0300

    clean: declarative bridge network instead shellscript check

--- docker.sh
+++ docker.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-NET='evolution-net'
 IMAGE='evolution/api:local'
-
-if !(docker network ls | grep ${NET} > /dev/null)
-then
-  docker network create -d bridge ${NET}
-fi
 
 docker build -t ${IMAGE} .
 
