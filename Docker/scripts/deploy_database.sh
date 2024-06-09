585692cae3d6e2981825118e7a8d371d6c659295
Davidson Gomes <davidsongviolao@gmail.com>
2024-06-09 16:15:44 -0300

    git actions v2

--- Docker/scripts/deploy_database.sh
+++ Docker/scripts/deploy_database.sh
@@ -8,8 +8,8 @@ fi
 
 if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ]]; then
     export DATABASE_URL
-    prisma migrate deploy --schema=prisma/$DATABASE_PROVIDER-schema.prisma
-    prisma generate --schema=prisma/$DATABASE_PROVIDER-schema.prisma
+    ./node_modules/.bin/prisma migrate deploy --schema=prisma/$DATABASE_PROVIDER-schema.prisma
+    ./node_modules/.bin/prisma generate --schema=prisma/$DATABASE_PROVIDER-schema.prisma
 else
     echo "Error: Database provider $DATABASE_PROVIDER invalid."
     exit 1
--- Docker/scripts/generate_database.sh
+++ Docker/scripts/generate_database.sh
@@ -9,7 +9,7 @@ fi
 if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ]]; then
     export DATABASE_URL
     echo "Database URL: $DATABASE_URL"
-    prisma generate --schema=prisma/$DATABASE_PROVIDER-schema.prisma
+    ./node_modules/.bin/prisma generate --schema=prisma/$DATABASE_PROVIDER-schema.prisma
 else
     echo "Error: Database provider $DATABASE_PROVIDER invalid."
     exit 1
