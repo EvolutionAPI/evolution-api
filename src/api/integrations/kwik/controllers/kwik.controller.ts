d9fd6e9190bc30ee649f031f3c92b8345b67fc4e
Lucas Batista <lucas.batista@instant.com.br>
2024-09-09 17:51:08 -0300

    update edit mode 2

--- src/api/integrations/kwik/controllers/kwik.controller.ts
+++ src/api/integrations/kwik/controllers/kwik.controller.ts
@@ -12,7 +12,7 @@ export class KwikController {
   constructor(private readonly waMonitor: WAMonitoringService) {}
 
   private isTextMessage(messageType: any) {
-    return ['senderKeyDistributionMessage', 'conversation', 'extendedTextMessage', 'protocolMessage'].includes(messageType);
+    return ['senderKeyDistributionMessage', 'conversation', 'extendedTextMessage', 'protocolMessage', 'messageContextInfo'].includes(messageType);
   }
   public async fetchChats(
     { instanceName }: InstanceDto,
