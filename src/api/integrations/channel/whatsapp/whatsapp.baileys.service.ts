c4ddfe6804bf51585ff1a21854f05a4e6867b66e
edisoncm-ti <edisoncm.ti@gmail.com>
2025-05-07 18:18:47 -0300

    style: fix linting issues with Prettier

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -3005,15 +3005,24 @@ export class BaileysStartupService extends ChannelStartupService {
           .audioFrequency(48000)
           .audioChannels(1)
           .outputOptions([
-            '-write_xing', '0',
-            '-compression_level', '10',
-            '-application', 'voip',
-            '-fflags', '+bitexact',
-            '-flags', '+bitexact',
-            '-id3v2_version', '0',
-            '-map_metadata', '-1',
-            '-map_chapters', '-1',
-            '-write_bext', '0'
+            '-write_xing',
+            '0',
+            '-compression_level',
+            '10',
+            '-application',
+            'voip',
+            '-fflags',
+            '+bitexact',
+            '-flags',
+            '+bitexact',
+            '-id3v2_version',
+            '0',
+            '-map_metadata',
+            '-1',
+            '-map_chapters',
+            '-1',
+            '-write_bext',
+            '0',
           ])
           .pipe(outputAudioStream, { end: true })
           .on('error', function (error) {
