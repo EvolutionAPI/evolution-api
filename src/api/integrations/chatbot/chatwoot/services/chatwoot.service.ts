8b0b59d8fb9c021843a2f4a60033b7f15d21c804
Jose Sanchis <38044642+minarkap@users.noreply.github.com>
2025-07-16 11:21:19 +0200

    Update chatwoot.service.ts

--- src/api/integrations/chatbot/chatwoot/services/chatwoot.service.ts
+++ src/api/integrations/chatbot/chatwoot/services/chatwoot.service.ts
@@ -26,7 +26,7 @@ import axios from 'axios';
 import { proto } from 'baileys';
 import dayjs from 'dayjs';
 import FormData from 'form-data';
-import Jimp from 'jimp';
+import Jimp, { MIME_PNG } from 'jimp';
 import Long from 'long';
 import mimeTypes from 'mime-types';
 import path from 'path';
@@ -2103,7 +2103,7 @@ export class ChatwootService {
           const img = await Jimp.read(fileData);
           await img.cover(320, 180);
 
-          const processedBuffer = await img.getBufferAsync(Jimp.MIME_PNG);
+          const processedBuffer = await img.getBufferAsync(MIME_PNG);
 
           const fileStream = new Readable();
           fileStream._read = () => {}; // _read is required but you can noop it
