6564c162448c7ddf7fe7c69adca7d1bdc374c618
Jose Sanchis <38044642+minarkap@users.noreply.github.com>
2025-07-16 11:45:31 +0200

    Update chatwoot.service.ts

--- src/api/integrations/chatbot/chatwoot/services/chatwoot.service.ts
+++ src/api/integrations/chatbot/chatwoot/services/chatwoot.service.ts
@@ -2101,7 +2101,7 @@ export class ChatwootService {
           const fileData = Buffer.from(imgBuffer.data, 'binary');
 
           const img = await Jimp.read(fileData);
-          await img.cover(320, 180);
+          img.cover({ w: 320, h: 180 });
 
           const processedBuffer = await img.getBuffer(JimpMime.png);
 
