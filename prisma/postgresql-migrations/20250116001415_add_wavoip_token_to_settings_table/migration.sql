6e652d6ea25819fdfe22e398a6b0c61c07db0360
Caíque Zanetoni Fim <50633626+caiquezanetoni@users.noreply.github.com>
2025-08-18 08:43:21 -0300

    Update migration.sql

--- prisma/postgresql-migrations/20250116001415_add_wavoip_token_to_settings_table/migration.sql
+++ prisma/postgresql-migrations/20250116001415_add_wavoip_token_to_settings_table/migration.sql
@@ -6,14 +6,4 @@ Warnings:
 */
 
 -- AlterTable
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'Setting'
-        AND column_name = 'wavoipToken'
-    ) THEN
-        ALTER TABLE "Setting" ADD COLUMN "wavoipToken" VARCHAR(100);
-    END IF;
-END $$;
+ALTER TABLE "Setting" ADD COLUMN IF NOT EXISTS "wavoipToken" VARCHAR(100);
